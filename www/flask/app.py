20b6d6159b5ff158155ec58427ae26cc97ced971
Tony Fischetti <tony.fischetti@gmail.com>
2022-02-11 10:26:18 -0500

    fixed issue with routes not being hardcoded enough

--- www/flask/app.py
+++ www/flask/app.py
@@ -21,19 +21,6 @@ def index():
 #                            debug=debug_p)
 
 
-@app.route('/bikini-atoll', methods=['GET'])
-def bikini_atoll():
-    chain       = request.args.get('chain', default="harmony", type=str)
-    con_address = request.args.get('address', type=str,
-                                   default="0x1b1f9e1227aae2a67f03955f222d7642e2ba1720")
-    sver        = request.args.get('version', default="v6", type=str)
-    debug_p     = request.args.get('debug', default="false", type=str)
-    return render_template("microblog.html",
-                           chain=chain,
-                           contract=con_address,
-                           version=sver,
-                           debug=debug_p)
-
 @app.route('/tune', methods=['GET'])
 def tune():
     chain       = request.args.get('chain', default="harmony", type=str)
@@ -50,28 +37,28 @@ def tune():
 # pinned (version 6)
 @app.route('/den-of-understanding', methods=['GET'])
 def currentden6():
-    chain       = request.args.get('chain', default="harmony", type=str)
-    con_address = request.args.get('address', type=str,
-                                   default="0xd63b063df87e53a60feca6001375977d66f54083")
-    sver        = request.args.get('version', default="v6", type=str)
     debug_p     = request.args.get('debug', default="false", type=str)
     scrollto    = request.args.get('scrollto', default=None, type=str)
-    return render_template("microblog.html", chain=chain,
-                           contract=con_address, version=sver, debug=debug_p,
-                           scrollto=scrollto)
+    return render_template("microblog.html", chain="harmony",
+                           contract="0xd63b063df87e53a60feca6001375977d66f54083",
+                           version="v6", debug=debug_p, scrollto=scrollto)
+
+@app.route('/bikini-atoll', methods=['GET'])
+def bikini_atoll():
+    debug_p     = request.args.get('debug', default="true", type=str)
+    scrollto    = request.args.get('scrollto', default=None, type=str)
+    return render_template("microblog.html", chain="harmony",
+                           contract="0x1b1f9e1227aae2a67f03955f222d7642e2ba1720",
+                           version="v6", debug=debug_p, scrollto=scrollto)
 
 
 @app.route('/arsenic!', methods=['GET'])
 def arsenic_pinned():
-    chain       = request.args.get('chain', default="harmony", type=str)
-    con_address = request.args.get('address', type=str,
-                                   default="0x82419640d0fcc1bedaf4c3875b29be202bf0cce3")
-    sver        = request.args.get('version', default="v6", type=str)
     debug_p     = request.args.get('debug', default="false", type=str)
     scrollto    = request.args.get('scrollto', default=None, type=str)
-    return render_template("microblog.html", chain=chain,
-                           contract=con_address, version=sver, debug=debug_p,
-                           scrollto=scrollto)
+    return render_template("microblog.html", chain="harmony",
+                           contract="0x82419640d0fcc1bedaf4c3875b29be202bf0cce3",
+                           version="v6", debug=debug_p, scrollto=scrollto)
 
 
 
