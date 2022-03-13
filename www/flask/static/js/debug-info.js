7102bcb8d87c7f2632c3f029f345f434702c3539
Tony Fischetti <tony.fischetti@gmail.com>
2022-03-13 15:53:57 -0400

    great css additions

--- www/flask/static/js/debug-info.js
+++ www/flask/static/js/debug-info.js
@@ -3,17 +3,23 @@
 
 const begin = () => {
   fillMobileOrDesktop();
-  document.getElementById("datenow").textContent = `Date.now(): ${Date.now()}`;
+  document.getElementById("date-now").textContent = `Date.now(): ${Date.now()}`;
+  // TODO: the viewport size because I has about
+  //       to write mobile only CSS
+  //       and I cleaned up the the id/class names
+  //       WOW
 };
 
 const fillMobileOrDesktop = () => {
-  let target = document.getElementById("is-mobile-or-desktop-div");
-  target.innerHTML = `is mobile: ${IS_MOBILE_P}`;
-};
+  document.getElementById("is-mobile-or-desktop-div").target.innerHTML =
+    `is mobile: ${IS_MOBILE_P}`;
+
 
 // TODO: this
 // const attemptToGetGeoLocation = () => {
 //   const jsonpCallback = () => {
+  //
+  //   llback = () => {
 //     alert('Latitude: ' + data.latitude +
 //       '\nLongitude: ' + data.longitude +
 //       '\nCountry: ' + data.address.country);
