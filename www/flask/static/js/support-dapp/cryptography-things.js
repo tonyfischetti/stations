625834648855024ee0a628e7e1916d5814cccd56
Tony Fischetti <tony.fischetti@gmail.com>
2022-04-20 10:12:42 -0400

    now uses JSON.parse in chain-info.js

--- www/flask/static/js/support-dapp/cryptography-things.js
+++ www/flask/static/js/support-dapp/cryptography-things.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as crypto from '../dependencies/openpgp.mjs';
+// import * as crypto from '../dependencies/openpgp.mjs';
 
 // window._DEBUG("[crypto] hi from crypto");
 console.log("hi from crypto");
